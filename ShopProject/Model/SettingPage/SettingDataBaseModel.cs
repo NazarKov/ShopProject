4bb977008a01f234ad2781ff8aab1a70c9a92415
NazarKov <nazar.korniychuk@ukr.net>
2023-01-16 21:07:40 +0200

    Update Setting DataBase Fixed Bag Create and Delete DataBase Update Class SettingDataBaseModel Create audit Exeptions

---
+++ ShopProject/Model/SettingPage/SettingDataBaseModel.cs
@@ -0,0 +1,160 @@
+﻿using ShopProject.DataBase.Context;
+using ShopProject.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace ShopProject.Model.SettingPage
+{
+    internal class SettingDataBaseModel 
+    {
+        private XmlSerializer xmlSerializer;
+        private ResourceModel setting;
+        private ShopContext? db;
+        private Configuration configuration;
+        private ConnectionStringSettings? connectionString;
+
+        private string? nameDataBase;
+
+        public SettingDataBaseModel()
+        {
+            setting = new ResourceModel();
+            db = new ShopContext();
+            xmlSerializer = new XmlSerializer(typeof(ResourceModel));
+            configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            FileOpenOptionsDataBase();
+            this.nameDataBase = setting.NameDb;
+        }
+
+        public bool CreateDataBase()
+        {
+            try
+            {
+                if (db != null)
+                {
+                    db.Database.Create();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                HendlerShowExeption(ex); return false;
+            }
+            return false;
+        }
+
+        public bool DeleteDataBase()
+        {
+            try
+            {
+                if (db != null)
+                {
+                    return db.Database.Delete();
+                }
+            }
+            catch(Exception ex)
+            {
+                HendlerShowExeption(ex);
+                return false;
+            }
+            return false;
+        }
+      
+        private void HendlerShowExeption(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Помилка", MessageBoxButton.OK);
+
+        }
+
+        public void SetName(string name)
+        {
+            this.nameDataBase = name;
+            setting.SetNameDb(name);
+        }
+
+        public string GetName()
+        {
+            if (nameDataBase != null)
+                return nameDataBase;
+            else
+                return string.Empty;
+        }
+        
+        public bool SetConnectionStringDataBase()
+        {
+            try
+            {        
+                RemoveConnectingString(configuration);
+                
+                configuration.ConnectionStrings.ConnectionStrings.Add(CreateConnectionString());
+                configuration.Save(ConfigurationSaveMode.Modified, true);
+
+                ConfigurationManager.RefreshSection("connectionStrings");
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                HendlerShowExeption(ex);
+                return false;
+            }
+        }
+
+        private void RemoveConnectingString(Configuration configuration)
+        {
+            for (int i = 0, maxCount = configuration.ConnectionStrings.ConnectionStrings.Count-1; i < maxCount; i++)
+            {
+                configuration.ConnectionStrings.ConnectionStrings.RemoveAt(i);
+            }
+        }
+        
+        private ConnectionStringSettings CreateConnectionString()
+        {
+            connectionString = new ConnectionStringSettings();
+            connectionString.ConnectionString = "Data Source=" + Environment.MachineName + ";Initial Catalog=" + this.nameDataBase + ";Integrated Security=True";
+            connectionString.Name = "DBConnectionMyString";
+            connectionString.ProviderName = "System.Data.SqlClient";
+
+            return connectionString;
+        }
+
+        public void FileSaveOptionsDataBase()
+        {
+            using (FileStream fs = new FileStream(@"..\..\..\Resource\appSetting.xml", FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, setting);
+            }
+        }
+       
+        private void FileOpenOptionsDataBase()
+        {
+            using (FileStream fs = new FileStream(@"..\..\..\Resource\appSetting.xml", FileMode.OpenOrCreate))
+            {
+                setting =(ResourceModel)xmlSerializer.Deserialize(fs);
+            }
+        }
+
+        public void RemoveSetting()
+        {
+            try
+            {
+                setting = new ResourceModel();
+                FileSaveOptionsDataBase();
+                RemoveConnectingString(configuration);
+
+            }
+            catch (Exception ex)
+            {
+                HendlerShowExeption(ex);
+            }
+        }
+
+    }
+}
---
+++ ShopProject/ViewModel/SettingPage/SettingDataBaseViewModel.cs
@@ -0,0 +1,147 @@
+﻿using ShopProject.DataBase.Context;
+using ShopProject.Model;
+using ShopProject.Model.SettingPage;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
+
+namespace ShopProject.ViewModel.SettingPage
+{
+    internal class SettingDataBaseViewModel : ViewModel<SettingDataBaseViewModel>
+    {
+        private ICommand createDataBase;
+        private ICommand deleteDataBase;
+
+        SettingDataBaseModel settingDb;
+     
+        public SettingDataBaseViewModel()
+        {
+            settingDb = new SettingDataBaseModel();
+            createDataBase = new DelegateCommand(CreateDataBaseInUpdateField);
+            deleteDataBase = new DelegateCommand(DeleteDb);
+           
+            _isCreateButton = true;
+            _dbname = string.Empty;
+            _isCreateLableName = string.Empty;
+
+            if (settingDb.GetName() != string.Empty)
+            {
+                SetFieldText(true);         
+            }
+            else
+            {
+                SetFieldText(false);
+            }
+        }
+
+        private void SetFieldText(bool createTrueAndFalse)
+        {
+            if (createTrueAndFalse)
+            {
+                IsCreateButton = false;
+                IsCreateLableName = "База створена";
+                DBName = settingDb.GetName();
+            }
+            else
+            {
+                IsCreateButton = true;
+                IsCreateLableName =string.Empty;
+                DBName = string.Empty;
+            }
+        }
+
+        private string _dbname;
+        public string DBName
+        {
+            get { return _dbname; }
+            set { _dbname = value; }
+        }
+
+        private string _isCreateLableName;
+        public string IsCreateLableName
+        {
+            get { return _isCreateLableName; }
+            set { _isCreateLableName = value; OnPropertyChanged("IsCreateLableName");}
+        }
+
+        private bool _isCreateButton;
+        public bool IsCreateButton
+        {
+            get { return _isCreateButton; }
+            set { _isCreateButton = value; OnPropertyChanged("IsCreateButton"); }
+        }
+
+        public ICommand CreateDataBase => createDataBase;
+
+        private void CreateDataBaseInUpdateField()
+        {
+            settingDb.SetName(_dbname);
+            if (settingDb.SetConnectionStringDataBase())
+            {
+                new Thread(new ThreadStart(ThreadCreateDataBase)).Start();
+                SaveFileSettingDataBase();
+            }
+            else
+            {
+                MessageBox.Show("База даних не створена","Eror",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+        
+        void ThreadCreateDataBase()
+        {
+            if (settingDb.CreateDataBase())
+            {
+                UpdateField();
+                MessageBox.Show("База даних створена", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        void UpdateField()
+        {
+            IsCreateLableName = "База створена";
+            IsCreateButton = false;
+        }
+        private void SaveFileSettingDataBase()
+        {
+            if (_dbname != null)
+            {
+                settingDb.SetName(_dbname);
+                settingDb.FileSaveOptionsDataBase();
+            }
+            else
+            {
+                GenerateException("Введіть назву бази даних");
+            }
+        }
+
+        private Exception GenerateException(string nameExeption)
+        {
+            throw new Exception(nameExeption);
+        }
+
+        public ICommand DeleteDataBase => deleteDataBase;
+
+        private void DeleteDb()
+        {
+            new Thread(new ThreadStart(deleteDb)).Start();
+            settingDb.RemoveSetting();
+        }
+        void deleteDb()
+        {
+            if (settingDb.DeleteDataBase())
+            {
+                SetFieldText(false);
+                MessageBox.Show("База даних видалена", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+
+    }
+}
