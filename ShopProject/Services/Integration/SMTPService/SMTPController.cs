c7c07b9b48e7df06b3f5a302ed6190517f6894b8
назар корнійчук <nazar.korniychuk@ukr.net>
2026-04-19 01:10:31 +0300

    delete mailkit

--- ShopProject/Services/Integration/SMTPService/SMTPController.cs
+++ ShopProject/Services/Integration/SMTPService/SMTPController.cs
@@ -1,5 +1,4 @@
-﻿using MailKit.Net.Smtp;
-using MimeKit;
+﻿
 
 namespace ShopProject.Services.Integration.SMTPService
 {
@@ -18,51 +17,51 @@ namespace ShopProject.Services.Integration.SMTPService
         private readonly int[] PortGmail = { 25, 465, 587 };
         private readonly int PortUkrNet = 465;
 
-        private MimeMessage Message;
+//        private MimeMessage Message;
 
         public SMTPController()
         {
-            Message = new MimeMessage();
+            //Message = new MimeMessage();
         }
 
         public void SendMessage(string emailAddres, string codeChangePassword, TypeSMPTServer typeSMPT)
         {
-            Message = new MimeMessage();
-            using (var smtp = new SmtpClient())
-            {
-                Message.To.Add(new MailboxAddress("Receiver Name", emailAddres));
+            //Message = new MimeMessage();
+            //using (var smtp = new SmtpClient())
+            //{
+            //    Message.To.Add(new MailboxAddress("Receiver Name", emailAddres));
 
-                Message.Subject = "Change password";
+            //    Message.Subject = "Change password";
 
-                Message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-                {
-                    Text = "<b>Код підтвердження для зміни пароля " + codeChangePassword + "</b>"
-                };
+            //    //Message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            //    //{
+            //    //    Text = "<b>Код підтвердження для зміни пароля " + codeChangePassword + "</b>"
+            //    //};
 
 
-                switch (typeSMPT)
-                {
-                    case TypeSMPTServer.Gmail:
-                        {
-                            Message.From.Add(new MailboxAddress("ShopProject", SenderEmailGmail));
+            //    switch (typeSMPT)
+            //    {
+            //        case TypeSMPTServer.Gmail:
+            //            {
+            //               // Message.From.Add(new MailboxAddress("ShopProject", SenderEmailGmail));
 
-                            smtp.Connect(SMTPGmail, PortGmail[1], true);
-                            smtp.Authenticate(SenderEmailGmail, SenderPasswordGmail);
-                            break;
-                        }
-                    case TypeSMPTServer.UkrNet:
-                        {
-                            Message.From.Add(new MailboxAddress("ShopProject", SenderEmailUkrNet));
+            //                smtp.Connect(SMTPGmail, PortGmail[1], true);
+            //                smtp.Authenticate(SenderEmailGmail, SenderPasswordGmail);
+            //                break;
+            //            }
+            //        case TypeSMPTServer.UkrNet:
+            //            {
+            //                //Message.From.Add(new MailboxAddress("ShopProject", SenderEmailUkrNet));
 
-                            smtp.Connect(SMTPUkrNet, PortUkrNet, true);
-                            smtp.Authenticate(SenderEmailUkrNet, SenderPasswordUkrNet);
-                            break;
-                        }
-                }
+            //                smtp.Connect(SMTPUkrNet, PortUkrNet, true);
+            //                smtp.Authenticate(SenderEmailUkrNet, SenderPasswordUkrNet);
+            //                break;
+            //            }
+            //    }
 
-                smtp.Send(Message);
-                smtp.Disconnect(true);
-            }
+            //    smtp.Send(Message);
+            //    smtp.Disconnect(true);
+            //}
 
         }
 
